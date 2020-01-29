fc3b1bf61c8a350c68ee39cdd757ab0cb08f3f86
AriizumiKW <48888335+AriizumiKW@users.noreply.github.com>
2020-01-29 23:08:31 +0000

    OpenCVSharp-First-Commit

---
+++ Assets/Main/Script/OpenCVDemo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenCvSharp;
+
+public class OpenCVDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mat i = Cv2.ImRead(Application.persistentDataPath + "/pic.jpg");
+        //Debug.Log(Application.persistentDataPath);
+        //using (new Window("src image", i)) Cv2.WaitKey();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

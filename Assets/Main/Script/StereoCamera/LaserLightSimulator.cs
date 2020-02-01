477e88195a3fa7b1245e358971694aab4e238f44
AriizumiKW <48888335+AriizumiKW@users.noreply.github.com>
2020-02-01 20:49:39 +0000

    StereoCamera-2
    environment and laser

---
+++ Assets/Main/Script/StereoCamera/LaserLightSimulator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserLightSimulator : MonoBehaviour
+{
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Ray r = new Ray(this.transform.position, transform.forward);
+        RaycastHit hitInfo;
+        if(Physics.Raycast(r, out hitInfo))
+        {
+            distance = (r.origin - hitInfo.point).magnitude;
+            this.gameObject.GetComponent<Light>().range = distance + 1.0f;
+
+            //Debug.Log(distance);
+            Debug.DrawLine(r.origin, hitInfo.point);
+        }
+    }
+}
